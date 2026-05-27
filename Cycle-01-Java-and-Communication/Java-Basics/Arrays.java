2230b7bfe111cc0ce2e70737ad29f85776986366
VISHAL 001 <rawatvishal2025@gmail.com>
2026-05-27 10:08:17 +0530

    Add Arrays class to demonstrate array usage

---
+++ Cycle-01-Java-and-Communication/Java-Basics/Arrays.java
@@ -0,0 +1,15 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Array Elements:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        System.out.println("\nArray Length:");
+        System.out.println(numbers.length);
+    }
+}
